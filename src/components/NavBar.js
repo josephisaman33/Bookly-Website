cdb40d7b9811bc11548cc245666b6f712008dca3
casma103 <41581802+chrisasma103@users.noreply.github.com>
2023-03-15 11:24:05 -0400

    Added search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function NavBar() {
   return (
@@ -15,11 +17,20 @@ function NavBar() {
             <Nav.Link href="home">Home</Nav.Link>
             <Nav.Link href="bookshelf">Bookshelf</Nav.Link>
             <Nav.Link href="bookrec">Book Recommendation</Nav.Link>
-            <Nav.Link href="library">Library</Nav.Link>
+            <Nav.Link href="library">Library Search</Nav.Link>
             <Nav.Link href="reflection">Reflection</Nav.Link>
             <Nav.Link href="account">Account</Nav.Link>
           </Nav>
         </Container>
+      <Form className="d-flex">
+        <Form.Control
+          type="search"
+          placeholder="Find a Book"
+          className="me-2"
+          aria-label="Search"
+          />
+        <Button variant="info">Search</Button>
+      </Form>
       </Navbar>
     </>
   );
