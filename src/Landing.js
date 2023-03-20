2c433a1e110fb8c12dbd080ff9524a4a8faa3d53
casma103 <41581802+chrisasma103@users.noreply.github.com>
2023-03-20 14:14:44 -0400

    Added home button

--- src/Landing.js
+++ src/Landing.js
@@ -1,9 +1,10 @@
-import Image from "react-bootstrap/Image";
 import "bootstrap/dist/css/bootstrap.css";
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 function Landing() {
     return (
+      <>
         <Card className="bg-dark text-white">
           <Card.Img src="https://www.sarawhitley.com/wp-content/uploads/2019/03/cropped-blur-book-stack-books-2.jpg" alt="Blurred Book Background" fluid="true" />
           <Card.ImgOverlay>
@@ -12,7 +13,14 @@ function Landing() {
             </Card>
           </Card.ImgOverlay>
         </Card>
-      );
-    }
+        
+        <div className="d-grid gap-1">
+          <Button variant="primary" size="lg" href="home">
+            Get Started Now
+          </Button>
+        </div>
+      </>
+    );
+}
 
 export default Landing;
