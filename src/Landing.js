7d6830c3439636f031e655a77d05ff8779d60339
casma103 <41581802+chrisasma103@users.noreply.github.com>
2023-03-21 10:49:43 -0400

    Replaced filler text with Lorem Ipsum

--- src/Landing.js
+++ src/Landing.js
@@ -14,19 +14,8 @@ function Landing() {
                 <h1 className='info-header' data-aos="fade-up" data-aos-delay="600">Read A Book</h1>
                 <div className='info-section>'>
                     <img src={bookly_pic} alt='bookly_pic' className='bookly-pic' data-aos="fade-right"/>
-                    <p className='info' data-aos="fade-left">jklsdaasjdfkasdfkasfdjkjkasdf
-                        asdkl;f;laskdfl;kajsdf;ljka;lsdjl;
-                        sad;klfjsadl;kfjasl;kdfjsakfl;
-                        asdf;klasjdfl;kasjdf;kasl;dfjsdf
-                        asl;dkfjasl;kdfj;lksdjsdfsfasjdakls
-                        askdjaskldjkl;asdj;laksjd;lakjsdkasd
-                        dsjhfsjdfhjsfdjhfsdjhsffjksdfh
-                        asdjihfsdfhjjdfkshjkhdfshjkdfsjkfsdhjkfsdjkkhj
-                        asdf;klasjdfl;kasjdf;kasl;dfjsdf
-                        asl;dkfjasl;kdfj;lksdjsdfsfasjdakls
-                        askdjaskldjkl;asdj;laksjd;lakjsdkasd
-                        dsjhfsjdfhjsfdjhfsdjhsffjksdfh
-                        asdjihfsdfhjjdfkshjkhdfshjkdfsjkfsdhjkfsdjkkhj
+                    <p className='info' data-aos="fade-left">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis blandit turpis cursus in hac habitasse. Eget dolor morbi non arcu. Dolor sit amet consectetur adipiscing elit. Quis blandit turpis cursus in hac. Lectus nulla at volutpat diam ut venenatis tellus in metus. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida. Aliquam ultrices sagittis orci a scelerisque purus semper. Quisque non tellus orci ac auctor. Amet dictum sit amet justo donec enim diam.
                     </p>
                 </div>
             </div>
