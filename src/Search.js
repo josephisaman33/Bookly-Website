cfafbb357af25cefef7524f3931fca3be27af9e0
casma103 <41581802+chrisasma103@users.noreply.github.com>
2023-03-26 16:23:36 -0400

    did some work on implementing APIs

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,35 @@
-function Search() {
+/*function Search() {
     return(
         <div>
             <h1>Search Results</h1>
         </div>
     )
 }
+*/
+import { useState, useEffect } from 'react';
+const App = () => {
+    const [posts, setPosts] = useState([]);
 
+    useEffect(() => {
+        fetch('https://openlibrary.org/search.json?title=where+the+crawdads+sing')
+            .then((res) => res.json())
+            .then((data) => {
+                //console.log(data);
+                setPosts(data);
+            })
+            .catch((err) => {
+                //console.log(err.message);
+            });
+    }, []);
+    //console.log(posts.docs[0])
+    console.log(posts.docs)
+    return(null);
+}
+function Search(){
+    return(
+        <div>
+            <h1>Test:<App></App></h1>
+        </div>
+    );
+}
 export default Search;
