a8eb6d5155d5140c2b76671b705d694c8bfea9d2
Joseph Isaman <joseph.isaman33@myhunter.cuny.edu>
2023-04-10 13:46:05 -0400

    Copied Backend for Recommendation System

--- src/BookRec.js
+++ src/BookRec.js
@@ -1,7 +1,8 @@
 import { BookCards } from './components/BookRec/bookcards';
 import basis from './components/BookRec/bookRecBasisData';
 import contents from './components/BookRec/bookRecContentData';
-import Table from 'react-bootstrap/Table'
+import {Table} from 'react-bootstrap'
+// import {React, useState} from 'react'
 
 function BookRec() {
     var originals = basis.map(basis => (
@@ -31,7 +32,7 @@ function BookRec() {
                     <tr>
                         <td>{originals[0]}</td>
                         <td style={{verticalAlign: "middle"}}><button>&#8249;</button></td>
-                        <td>{recommendations[0]}</td>
+                        <div onClick={() => alert("Hello from here")}><td>{recommendations[0]}</td></div>
                         <td>{recommendations[1]}</td>
                         <td>{recommendations[2]}</td>
                         <td>{recommendations[3]}</td>
@@ -60,6 +61,50 @@ function BookRec() {
                     </tr>
                 </tbody>
             </Table>
+
+
+            YOOOOO
+            <Table w-auto>
+                <thead>
+                    <tr>
+                        <th>Based On</th>
+                        <th colSpan={3}>We Recommend</th>
+                    </tr>
+                </thead>
+                <tbody style={{verticalAlign: "bottom"}}>
+                    <tr>
+                        <td>{originals[0]}</td>
+                        <td style={{verticalAlign: "middle"}}><button>&#8249;</button></td>
+                        <td><div className='container-fluid'><div className='row'>{recommendations.slice(0,5)}</div></div></td>
+                        <td style={{verticalAlign: "middle"}}><button>&#8250;</button></td>
+                    </tr>
+                    <tr>
+                        <td>{originals[1]}</td>
+                        <td style={{verticalAlign: "middle"}}><button>&#8249;</button></td>
+                        <td>{recommendations.slice(5,10)}</td>
+                        <td style={{verticalAlign: "middle"}}><button>&#8250;</button></td>
+                    </tr>
+                    <tr>
+                        <td>{originals[2]}</td>
+                        <td style={{verticalAlign: "middle"}}><button>&#8249;</button></td>
+                        <td>{recommendations.slice(10,15)}</td>
+                        <td style={{verticalAlign: "middle"}}><button>&#8250;</button></td>
+                    </tr>
+                </tbody>
+            </Table>
+
+            YOOOOOOOOOOOOOOOOOOOOOO
+
+            
+
+        
+
+        
+
+
+
+
+
         </div>
     )
 }
