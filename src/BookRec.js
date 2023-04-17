24d0d09128d7e2eefe8e06ec25a14036fd72f863
Joseph Isaman <joseph.isaman33@myhunter.cuny.edu>
2023-04-17 01:32:02 -0400

    Added popovers to Book Rec

--- src/BookRec.js
+++ src/BookRec.js
@@ -1,8 +1,8 @@
 import { BookCards } from './components/BookRec/bookcards';
 import basis from './components/BookRec/bookRecBasisData';
 import contents from './components/BookRec/bookRecContentData';
-import {Table} from 'react-bootstrap'
-// import {React, useState} from 'react'
+import {Table, Popover, OverlayTrigger, Button} from 'react-bootstrap'
+import {React} from 'react'
 
 function BookRec() {
     var originals = basis.map(basis => (
@@ -11,12 +11,31 @@ function BookRec() {
                         image={basis.image}
                         />
                     ))
+
     var recommendations = contents.map(contents => (
                             <BookCards
                                 key={contents.id}
                                 image={contents.image}
                             />
                           ))
+
+    const popover = (props) => (
+        <Popover id="popover-basic">
+          <Popover.Header>
+            <div>
+              <h4>{props.title}</h4>
+              <h5>{props.author}</h5>
+            </div>
+          </Popover.Header>
+          <Popover.Body>
+            {props.description}
+            <Button variant="outline-secondary">
+              Add to your bookshelf
+            </Button>
+          </Popover.Body>
+        </Popover>
+      );
+
 
     return(
         <div className='BookRec'>
@@ -32,78 +51,35 @@ function BookRec() {
                     <tr>
                         <td>{originals[0]}</td>
                         <td style={{verticalAlign: "middle"}}><button>&#8249;</button></td>
-                        <div onClick={() => alert("Hello from here")}><td>{recommendations[0]}</td></div>
-                        <td>{recommendations[1]}</td>
-                        <td>{recommendations[2]}</td>
-                        <td>{recommendations[3]}</td>
-                        <td>{recommendations[4]}</td>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[0])}><td>{recommendations[0]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[1])}><td>{recommendations[1]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[2])}><td>{recommendations[2]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[3])}><td>{recommendations[3]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="left" overlay={popover(contents[4])}><td>{recommendations[4]}</td></OverlayTrigger>
                         <td style={{verticalAlign: "middle"}}><button>&#8250;</button></td>
                     </tr>
                     <tr>
                         <td>{originals[1]}</td>
                         <td style={{verticalAlign: "middle"}}><button>&#8249;</button></td>
-                        <td>{recommendations[5]}</td>
-                        <td>{recommendations[6]}</td>
-                        <td>{recommendations[7]}</td>
-                        <td>{recommendations[8]}</td>
-                        <td>{recommendations[9]}</td>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[5])}><td>{recommendations[5]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[6])}><td>{recommendations[6]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[7])}><td>{recommendations[7]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[8])}><td>{recommendations[8]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="left" overlay={popover(contents[9])}><td>{recommendations[9]}</td></OverlayTrigger>
                         <td style={{verticalAlign: "middle"}}><button>&#8250;</button></td>
                     </tr>
                     <tr>
                         <td>{originals[2]}</td>
                         <td style={{verticalAlign: "middle"}}><button>&#8249;</button></td>
-                        <td>{recommendations[10]}</td>
-                        <td>{recommendations[11]}</td>
-                        <td>{recommendations[12]}</td>
-                        <td>{recommendations[13]}</td>
-                        <td>{recommendations[14]}</td>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[10])}><td>{recommendations[10]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[11])}><td>{recommendations[11]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[12])}><td>{recommendations[12]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="right" overlay={popover(contents[13])}><td>{recommendations[13]}</td></OverlayTrigger>
+                        <OverlayTrigger trigger="click" placement="left" overlay={popover(contents[14])}><td>{recommendations[14]}</td></OverlayTrigger>
                         <td style={{verticalAlign: "middle"}}><button>&#8250;</button></td>
                     </tr>
                 </tbody>
             </Table>
-
-
-            YOOOOO
-            <Table w-auto>
-                <thead>
-                    <tr>
-                        <th>Based On</th>
-                        <th colSpan={3}>We Recommend</th>
-                    </tr>
-                </thead>
-                <tbody style={{verticalAlign: "bottom"}}>
-                    <tr>
-                        <td>{originals[0]}</td>
-                        <td style={{verticalAlign: "middle"}}><button>&#8249;</button></td>
-                        <td><div className='container-fluid'><div className='row'>{recommendations.slice(0,5)}</div></div></td>
-                        <td style={{verticalAlign: "middle"}}><button>&#8250;</button></td>
-                    </tr>
-                    <tr>
-                        <td>{originals[1]}</td>
-                        <td style={{verticalAlign: "middle"}}><button>&#8249;</button></td>
-                        <td>{recommendations.slice(5,10)}</td>
-                        <td style={{verticalAlign: "middle"}}><button>&#8250;</button></td>
-                    </tr>
-                    <tr>
-                        <td>{originals[2]}</td>
-                        <td style={{verticalAlign: "middle"}}><button>&#8249;</button></td>
-                        <td>{recommendations.slice(10,15)}</td>
-                        <td style={{verticalAlign: "middle"}}><button>&#8250;</button></td>
-                    </tr>
-                </tbody>
-            </Table>
-
-            YOOOOOOOOOOOOOOOOOOOOOO
-
-            
-
-        
-
-        
-
-
-
-
 
         </div>
     )
--- src/components/BookRec/bookRecContentData.js
+++ src/components/BookRec/bookRecContentData.js
@@ -1,76 +1,121 @@
 const bookRecContentData = [
     {
         id: 0,
+        title:"It Ends With Us",
+        author: "Colleen Hoover",
+        description: "Lily hasn’t always had it easy, but that’s never stopped her from working hard for the life she wants. She’s come a long way from the small town in Maine where she grew up — she graduated from college, moved to Boston, and started her own business. So when she feels a spark with a gorgeous neurosurgeon named Ryle Kincaid, everything in Lily’s life suddenly seems almost too good to be true.",
         image: 
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1470427482i/27362503.jpg",
     },
     {
         id: 1,
+        title:"Verity",
+        author: "Colleen Hoover",
+        description: "Lowen Ashleigh is a struggling writer on the brink of financial ruin when she accepts the job offer of a lifetime. Jeremy Crawford, husband of bestselling author Verity Crawford, has hired Lowen to complete the remaining books in a successful series his injured wife is unable to finish.",
         image:
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1634158558i/59344312.jpg",
     },
     {
         id: 2,
+        title:"The Silent Patient",
+        author: "Alex Michaelides",
+        description: "Lowen Ashleigh is a struggling writer on the brink of financial ruin when she accepts the job offer of a lifetime. Jeremy Crawford, husband of bestselling author Verity Crawford, has hired Lowen to complete the remaining books in a successful series his injured wife is unable to finish.",
         image:
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1668782119i/40097951.jpg",
     },
     {
         id: 3,
+        title:"It Starts With Us",
+        author: "Coleen Hoover",
+        description: "Lowen Ashleigh is a struggling writer on the brink of financial ruin when she accepts the job offer of a lifetime. Jeremy Crawford, husband of bestselling author Verity Crawford, has hired Lowen to complete the remaining books in a successful series his injured wife is unable to finish.",
         image:
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1644605295i/60393672.jpg",
     },
     {
         id: 4,
+        title:"Twisted Love",
+        author: "Ana Huang",
+        description: "Alex Volkov is a devil blessed with the face of an angel and cursed with a past he can’t escape.\nDriven by a tragedy that has haunted him for most of his life, his ruthless pursuits for success and vengeance leave little room for matters of the heart.",
         image:
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1618487915i/55817097.jpg",
     },
     {
       id: 5,
+      title:"Wolverine Origins",
+      author: "Paul Jennkins",
+      description: "Collecting the best-selling - and controversial - romantic period piece that defied industry expectations and pulled back the curtain on Wolverine's mysterious past! At long last, all is revealed about the incredible forces that molded the world's most perfect killing machine with a heart as big as the great outdoors. Collects Wolverine: Origin #1-6",
       image:
         "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1320663605i/22469.jpg",
     },
     {
         id: 6,
+        title:"Thor Son of Asgard",
+        author: "Yoshida Tocchini",
+        description: "Explore the early ears of the God of Thunder in this adventure full of Marvel myth and magic! Join young Thor and his godly companions, Balder and Sif, as they travel the mystical land of Asgard on a quest of cosmic proportions. When Odin, the All-Father, sends his favorite son to gather four mystical elements that he will use to craft a powerful new weapon, not even Ragnarok will be able to stop the boy who would be king! Then, after their adventures across the wolds of Asgard, it's back to school for Thor, Balder and Sif...but when you're in training to be a warrior, study hall isn't so quiet! The lives of the three young friends take an interesting turn when Amora the Enchantress enters the picture. Sparks will fly when their schooling in swords and sorcery comes head-to-head!",
         image:
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1386923958i/7000729.jpg",
     },
     {
         id: 7,
+        title:"Watchmen",
+        author: "Alan Moore",
+        description: "This Hugo Award-winning graphic novel chronicles the fall from grace of a group of super-heroes plagued by all-too-human failings. Along the way, the concept of the super-hero is dissected as the heroes are stalked by an unknown assassin.\nOne of the most influential graphic novels of all time and a perennial best-seller, Watchmen has been studied on college campuses across the nation and is considered a gateway title, leading readers to other graphic novels such as V for Vendetta, Batman: The Dark Knight Returns and The Sandman series.",
         image:
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1442239711i/472331.jpg",
     },
     {
         id: 8,
+        title:"Lunar Storm",
+        author: "Terry Crosby",
+        description: "Missing Description",
         image:
           "https://www.adobe.com/express/create/cover/media_178ebed46ae02d6f3284c7886e9b28c5bb9046a02.jpeg?width=400&format=jpeg&optimize=medium",
     },
     {
         id: 9,
+        title:"The Outsider",
+        author: "Stephen King",
+        description: "An unspeakable crime. A confounding investigation. At a time when the King brand has never been stronger, he has delivered one of his most unsettling and compulsively readable stories.\nAn eleven-year-old boy’s violated corpse is found in a town park. Eyewitnesses and fingerprints point unmistakably to one of Flint City’s most popular citizens. He is Terry Maitland, Little League coach, English teacher, husband, and father of two girls. Detective Ralph Anderson, whose son Maitland once coached, orders a quick and very public arrest. Maitland has an alibi, but Anderson and the district attorney soon add DNA evidence to go with the fingerprints and witnesses. Their case seems ironclad.\nAs the investigation expands and horrifying answers begin to emerge, King’s propulsive story kicks into high gear, generating strong tension and almost unbearable suspense. Terry Maitland seems like a nice guy, but is he wearing another face? When the answer comes, it will shock you as only Stephen King can.",
         image:
           "https://www.skipprichard.com/wp-content/uploads/2018/12/9781501180989.jpg",
     },
     {
         id: 10,
+        title:"Trese Unreported Murders",
+        author: "Budjette Tan",
+        description: "When dusk arrives in the city of Manila, that's when you become the most likely prey of the criminal underworld. \n Kidnappers and thieves will be the least of your worries.",
         image:
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1277193939i/8490939.jpg",
     },
     {
         id: 11,
+        title:"Si Janus Sílang at ang Tiyanak ng Tábon",
+        author: "Edgar Samar",
+        description: "Sa tournament ng TALA Online sa bayan ng Balanga, namatay ang lahat ng manlalaro maliban kay Janus. Sunod-sunod pa ang naging kaso ng pagkamatay ng mga kabataan sa computer shops sa iba’t-ibang panig ng bansa. Kinontak si Janus ng nagpakilalang Joey, isa rin umano sa mga nakaligtas sa paglalaro ng TALA na gaya niya. Hindi inasahan ni Janus ang mga matutuklasan niya mula rito na mag-uugnay sa kanya sa misteryo ng kinahuhumalingan niyang RPG—at sa alamat ng Tiyanak mula sa Tábon!",
         image:
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1398428527i/22011229._SX300_.jpg",
     },
     {
         id: 12,
+        title:"Noli Me Tangrere",
+        author: "Jose Rizal",
+        description: "In more than a century since its appearance, José Rizal's Noli Me Tangere has become widely known as the great novel of the Philippines. A passionate love story set against the ugly political backdrop of repression, torture, and murder, \"The Noli,\" as it is called in the Philippines, was the first major artistic manifestation of Asian resistance to European colonialism, and Rizal became a guiding conscience—and martyr—for the revolution that would subsequently rise up in the Spanish province.",
         image:
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1309199939i/418285.jpg",
     },
     {
         id: 13,
+        title:"Rizal Without The Overcoat",
+        author: "Ambeth R. Ocampo",
+        description: "Through writing about history, Ocampo writes on Rizal as if he happened yesterday. In the clean, cool style of a good journalist...Ocampo is one histrian who has never been know to impose dogmas and definitive treatises. Reading Ocampo's history is like sitting down with a friend who shares what he has learned. But what he does best is to share the certainty of his doubts. Which probably makes him less of a historian. But the history is too serious to be left to historian.Desc The Silent",
         image:
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1617438104i/155795.jpg",
     },
     {
         id: 14,
+        title:"ABNKKBSNPLAKO?!",
+        author: "Bob Ong",
+        description: "Bakit namamalo si Miss Uyehara?May mga notebook bang lumilipad?\nBakit masakit sa ulo ang Mafhemafics?\nAno ang sikreto sa pagkakaibigan nila Pepe at Tagpi?\nBakit may mga taong nakapikit sa litrato?",
         image:
           "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1297789858i/1433337.jpg",
     }
