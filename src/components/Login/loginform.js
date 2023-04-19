d92c5c6de7de525e2f24af5e6e73bdb8d3110f84
Fruitcakeache <cchow10250@gmail.com>
2023-04-19 02:16:16 -0400

    form submission

--- src/components/Login/loginform.js
+++ src/components/Login/loginform.js
@@ -1,34 +1,55 @@
-import "./loginform.css"
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { useState } from "react";
+
+import "./loginform.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const LoginForm = () => {
-    return (
-        <div className="login-container">
-            <div className="cover">
-                <h1>Login</h1>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-                <Form action="#" className="login-form">
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email, password);
+  }
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+  return (
+    <div className="login-container">
+      <div className="cover">
+        <h1>Login</h1>
 
-                    <Button variant="primary" type="submit" className="login-btn">
-                        Login
-                    </Button>
-                </Form>
+        <Form action="#" className="login-form" onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
 
-                <a href="./Register"><h4>Register</h4></a>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
 
-            </div>
-        </div>
-    )
-}
+          <Button variant="primary" type="submit" className="login-btn">
+            Login
+          </Button>
+        </Form>
+
+        <a href="./Register">
+          <h4>Register</h4>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default LoginForm;
--- src/components/Register/registerform.js
+++ src/components/Register/registerform.js
@@ -1,38 +1,66 @@
-import "./registerform.css"
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { useState } from "react";
+
+import "./registerform.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const RegisterForm = () => {
-    return (
-        <div className="login-container">
-            <div className="cover">
-                <h1>Register</h1>
-                <Form action="#" className="login-form">
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email, password, phone);
+  }
 
-                    <Form.Group className="mb-3" controlId="formPhone">
-                        <Form.Label>Phone Number</Form.Label>
-                        <Form.Control type="tel" placeholder="Enter #" />
-                    </Form.Group>
+  return (
+    <div className="login-container">
+      <div className="cover">
+        <h1>Register</h1>
 
-                    <Button variant="primary" type="submit" className="login-btn">
-                        Register
-                    </Button>
-                </Form>
+        <Form action="#" className="login-form" onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
 
-                <a href="./Login"><h4>Back To Login</h4></a>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
 
-            </div>
-        </div>
-    )
-}
+          <Form.Group className="mb-3" controlId="formPhone">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control
+              type="tel"
+              placeholder="Enter #"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" className="login-btn">
+            Register
+          </Button>
+        </Form>
+
+        <a href="./Login">
+          <h4>Back To Login</h4>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default RegisterForm;
