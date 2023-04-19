009a655a3bed02d1e6ac0993c2165c025cf89ea3
Fruitcakeache <cchow10250@gmail.com>
2023-04-19 01:27:46 -0400

    updated to use react bootstrap form

--- src/components/Login/loginform.js
+++ src/components/Login/loginform.js
@@ -1,18 +1,30 @@
 import "./loginform.css"
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const LoginForm = () => {
     return (
         <div className="login-container">
             <div className="cover">
                 <h1>Login</h1>
-                <input type ="text" placeholder="Email"></input>
-                <input type ="password" placeholder="Password"></input>
-                    
-                <Button variant="primary" className='login-btn'>Login</Button>
 
-                <h4>Forgot Password?</h4>
-                <a href="./Register"><h3>Register</h3></a>
+                <Form action="#" className="login-form">
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit" className="login-btn">
+                        Login
+                    </Button>
+                </Form>
+
+                <a href="./Register"><h4>Register</h4></a>
 
             </div>
         </div>
--- src/components/Register/registerform.js
+++ src/components/Register/registerform.js
@@ -1,16 +1,32 @@
 import "./registerform.css"
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const RegisterForm = () => {
     return (
         <div className="login-container">
             <div className="cover">
                 <h1>Register</h1>
-                <input type ="text" placeholder="Email"></input>
-                <input type ="password" placeholder="Password"></input>
-                <input type ="phone" placeholder="Phone Number"></input>
-                    
-                <Button variant="primary" className='login-btn'>Register</Button>
+                <Form action="#" className="login-form">
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formPhone">
+                        <Form.Label>Phone Number</Form.Label>
+                        <Form.Control type="tel" placeholder="Enter #" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit" className="login-btn">
+                        Register
+                    </Button>
+                </Form>
 
                 <a href="./Login"><h4>Back To Login</h4></a>
 
