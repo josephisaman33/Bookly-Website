bbe69b9e55e3fc83cb10c1ee8062eeb8cdb03c98
ChristineRamiro <christineramiro1@gmail.com>
2023-05-03 09:25:42 -0400

    added a dropdown

---
+++ src/components/Notifications/Reminders.jsx
@@ -0,0 +1,22 @@
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import notifyme from './components/Notifications/NotifyMe';
+
+export default function NotificationSet() {
+  return (
+    <Dropdown as={ButtonGroup}>
+      <Button variant="secondary" onclick="notifyme()">Notify me</Button>
+
+      <Dropdown.Toggle split variant="secondary" id="dropdown-split-basic" />
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Every 3 hours</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Every 6 hours</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Every 12 hours</Dropdown.Item>
+        <Dropdown.Item href="#/action-4">Every 24 hours</Dropdown.Item>
+        <Dropdown.Item href="#/action-5">Every 48 hours</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
