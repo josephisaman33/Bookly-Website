445b2dea2ce41e2e125670423d8530c4bfa27708
yangyuchen <cyy200107@gmail.com>
2023-05-05 01:21:19 -0400

    cyy

--- src/Reflection.js
+++ src/Reflection.js
@@ -1,4 +1,36 @@
+import {useState} from "react";
+import axios from "axios";
+
 function Reflection() {
+
+    const [reflechion, setReflechion] = useState("");
+    const [rating, setRating] = useState(1);
+    const [bookName, setBookName] = useState("");
+
+    const onSubmit = function () {
+        if (reflechion == "") {
+            alert("please input your reflection!");
+            return;
+        }
+        if (bookName == "") {
+            alert("please input your favorite!");
+            return;
+        }
+        axios.post("http://localhost:8080/api/reflechionAndrating/submit",
+            {reflechion,rating,bookName})
+            .then(response => {
+                alert("success");
+                //Reset input data
+                setReflechion("");
+                setRating(1);
+                setBookName("");
+            })
+            .catch(() => {
+                alert("fail");
+                }
+            );
+    }
+
     return (
       <div>
         <div>
@@ -11,8 +43,7 @@ function Reflection() {
             <div class="span4">
               <h2></h2>
               <h3>Book description</h3>
-              <form action="/app1/reviews/" method="post">
-                <fieldset>
+
                   <br />
                   <input type="hidden" name="id" value="1" />
                   <label>Reflection：</label>
@@ -20,9 +51,13 @@ function Reflection() {
                     name="reflection"
                     class="form-control"
                     placeholder="give your reflection"
-                  ></textarea>
+                    value={reflechion}
+                    onChange={event => setReflechion(event.target.value)}>
+                  </textarea>
                   <label>Rating：</label>
-                  <select name="stars" class="form-control">
+                  <select name="stars" class="form-control"
+                          value={rating}
+                          onChange={event => setRating(event.target.value)}>
                     <option value="1">☆</option>
                     <option value="2">☆☆</option>
                     <option value="3">☆☆☆</option>
@@ -34,11 +69,13 @@ function Reflection() {
                     name="name"
                     class="form-control"
                     placeholder="give your favorite"
-                  ></textarea>
+                    value={bookName}
+                    onChange={event => setBookName(event.target.value)}>
+
+                  </textarea>
                   <br />
-                  <input type="submit" value="Submit" />
-                </fieldset>
-              </form>
+                <button onClick={onSubmit}>Submit</button>
+
             </div>
             <div class="span4"></div>
           </div>
